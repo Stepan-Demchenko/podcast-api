84a38304d1e86a6959099b77a9e3bede199c0e94
tar-aldev <frontdev.at@gmail.com>
2020-01-03 21:10:02 +0200

    added prettier config file

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -2,32 +2,32 @@ let jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
 let checkToken = (req, res, next) => {
-    let token = req.headers['x-access-token'] || req.headers['Authorization'];
+  let token = req.headers['x-access-token'] || req.headers['Authorization'];
 
-    if (token.startsWith('Bearer ')) {
-        token = token.slice(7, token.length);
-    }
+  if (token.startsWith('Bearer ')) {
+    token = token.slice(7, token.length);
+  }
 
-    if (token) {
-        jwt.verify(token, config.secretKey, (err, decoded) => {
-            if (err) {
-                return res.json({
-                    success: false,
-                    message: 'Token is not valid'
-                });
-            } else {
-                req.decoded = decoded;
-                next();
-            }
-        });
-    } else {
+  if (token) {
+    jwt.verify(token, config.secretKey, (err, decoded) => {
+      if (err) {
         return res.json({
-            success: false,
-            message: 'Auth token is not supplied'
-        })
-    }
+          success: false,
+          message: 'Token is not valid'
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.json({
+      success: false,
+      message: 'Auth token is not supplied'
+    });
+  }
 };
 
 module.exports = {
-    checkToken
+  checkToken
 };
