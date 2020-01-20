cbd7107e32a3de3b5c4bb45d6a72883b66bdc01e
tar-aldev <frontdev.at@gmail.com>
2020-01-20 12:50:01 +0200

    add podcast testing

---
+++ test/podcasts.test.js
@@ -0,0 +1,71 @@
+require('colors');
+const fs = require('fs');
+const request = require('supertest');
+const chai = require('chai');
+const expect = chai.expect;
+const app = require('../app');
+const { genereateToken } = require('../utils/generateToken');
+const mongoose = require('mongoose');
+const categoriesSeedingFunction = require('../seeding/categories.seed');
+const usersSeedingFunction = require('../seeding/users.seed');
+
+let categories;
+let users;
+let mockImagesPaths = [];
+let mockAudioPath = '';
+
+describe('Podcast controller test'.green.bold, () => {
+  beforeEach(done => {
+    mongoose.connection.db
+      .dropDatabase()
+      .then(() => {
+        return Promise.all([
+          categoriesSeedingFunction(),
+          usersSeedingFunction()
+        ]);
+      })
+      .then(([categoriesResp, usersResp]) => {
+        categories = categoriesResp;
+        users = usersResp;
+      })
+      .finally(done);
+  });
+
+  afterEach(() => {
+    if (fs.existsSync(mockAudioPath)) {
+      fs.unlinkSync(mockAudioPath);
+    }
+
+    mockImagesPaths.forEach(mockImagePath => {
+      if (fs.existsSync(mockImagePath)) {
+        fs.unlinkSync(mockImagePath);
+      }
+    });
+  });
+
+  it('Add podcast with token is successfull', () => {
+    return request(app)
+      .post('/api/podcasts')
+      .set({ authorization: `Bearer ${genereateToken(users[0])}` })
+      .field('title', 'test title')
+      .field('description', 'test description')
+      .field('categories[0]', `${categories[0]._id}`)
+      .field('categories[1]', `${categories[1]._id}`)
+      .attach('images', './test/fixtures/test-image.png')
+      .attach('audio', './test/fixtures/test.mp3')
+      .then(response => {
+        /* Save this to vars to delete test files */
+        console.log('response.body.data[0]', response.body.data[0]);
+        mockAudioPath = response.body.data[0].audioSrc;
+        mockImagesPaths = response.body.data[0].imagesSrc;
+
+        expect(response.status).to.eq(201);
+        expect(response.body.data[0]).to.deep.include({
+          title: 'test title',
+          description: 'test description',
+          categories: [`${categories[0]._id}`, `${categories[1]._id}`]
+        });
+        expect(response.body.data[0].imagesSrc.length).to.eq(1);
+      });
+  });
+});
